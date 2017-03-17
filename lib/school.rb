ff8e6e5333905ef7a41aad41eebc3975538e40db
Usman Pervaiz <De.Usman@yahoo.com>
2017-03-17 21:36:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,9 +2,28 @@ class School
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
 
   def roster
-    {}
+    @roster
   end
+
+  def add_student(name, grade)
+    if(roster[grade])
+      roster[grade].push(name)
+    else
+      roster[grade] = [name]
+    end
+  end
+  def grade(grade)
+    roster[grade]
+  end
+  def sort
+    roster.values.collect do |array|
+      array.sort!
+      end
+      roster
+  end
+
 end
